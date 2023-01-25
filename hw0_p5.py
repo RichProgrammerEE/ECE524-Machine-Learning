fdeca0055e11c442c146509de3485f200511bf64
Cole Richardson <cole.richardson12@gmail.com>
2023-01-24 21:04:26 -0500

    Update homework 0 based on quiz.

--- hw0_p5.py
+++ hw0_p5.py
@@ -8,19 +8,20 @@ if __name__ == "__main__":
 
     x = np.linspace(-5, 5, 1000)
     y = 1 / (1 + np.exp(-5 * (x - 1)))
+    # y = 1 / (1 + np.exp(-2 * (x + 1))) QUIZ
 
 
     try:
         # Plot the data
-        plt.figure(figsize=[12, 6])
+        fig = plt.figure(figsize=[12, 6])
         # colors: https://matplotlib.org/stable/tutorials/colors/colors.html
         plt.plot(x, y, "k", linewidth=6)
 
         plt.title(f"my plot")
-        # plt.legend()
         plt.ylabel("f(x)")
         plt.xlabel("x")
         # plt.grid(visible=True)
+        fig.savefig(f"hw0_p5.png")
         plt.show()
     except KeyboardInterrupt as keyerr:
         sys.exit()
